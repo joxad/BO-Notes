a15ed4778f3af122db1a8ba29c9e0b843cdaa6e8
Jocelyn David <jox.david@gmail.com>
2016-12-01 13:42:50 +0100

    First v of bo

---
+++ src/services/index.js
@@ -0,0 +1,13 @@
+'use strict';
+const note = require('./note');
+const authentication = require('./authentication');
+const user = require('./user');
+
+module.exports = function() {
+  const app = this;
+
+
+  app.configure(authentication);
+  app.configure(user);
+  app.configure(note);
+};
